060ef49226baab6b6a1a0bbaa70ce6351b9fae48
Donald Williams <60931728+Don-Williams@users.noreply.github.com>
2022-04-30 21:25:46 -0600

    Update MainActivity.kt
    interactive button

--- DiceRoller/app/src/main/java/williams/donald/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/williams/donald/diceroller/MainActivity.kt
@@ -1,11 +1,21 @@
 package williams.donald.diceroller
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.button)
+        rollButton.setOnClickListener {
+            Toast.makeText(this, "Dice rolled!", Toast.LENGTH_SHORT).show()
+            val resultTextView: TextView = findViewById(R.id.textView)
+            resultTextView.text = ((1..6).random()).toString()
+        }
     }
 }
